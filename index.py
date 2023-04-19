7b313175304f4e88520e2d672e5a6b024220da96
joy <yang10100520@gmail.com>
2023-04-20 01:36:45 +0800

    make it better

--- index.py
+++ index.py
@@ -5,15 +5,13 @@ import os
 openai.api_key = os.getenv("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")
 @app.route("/webhook", methods=["POST"])
 def webhook():
+    # build a request object
     req = request.get_json(force=True)
-    msg =  req["queryResult"]["queryText"]
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=msg,
-        max_tokens=100,
-        temperature=0.5,
-    )
-    info = "AI：" + response.choices[0].text
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
     return make_response(jsonify({"fulfillmentText": info}))
+
 if __name__ == "__main__":
     app.run()
