30b696d7bc62aa42d48e8241c8193d12d9ddc6e0
joy <yang10100520@gmail.com>
2023-04-19 18:40:55 +0800

    make it better

--- index.py
+++ index.py
@@ -1,17 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 app = Flask(__name__)
 import openai
 import os
 openai.api_key = os.getenv("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")
-@app.route("/")
-def index():
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    req = request.get_json(force=True)
+    msg =  req["queryResult"]["queryText"]
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="靜宜大學資管系楊子青老師在獲獎方面？",
-        max_tokens=128,
+        prompt=msg,
+        max_tokens=100,
         temperature=0.5,
     )
-    msg = response.choices[0].text
-    return msg
+    info = "AI：" + response.choices[0].text
+    return make_response(jsonify({"fulfillmentText": info}))
 if __name__ == "__main__":
     app.run()
