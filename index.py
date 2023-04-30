990c16f1b6992e86237557225e6ec370643b6a6c
joy <yang10100520@gmail.com>
2023-04-30 16:14:12 +0800

    make it better

--- index.py
+++ index.py
@@ -9,14 +9,9 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req.get("queryResult").get("action")
-
     if (action == "areaChoice"):
         area =  req.get("queryResult").get("parameters").get("area")
         info = "您選擇的旅遊區域是：" + area
-
-    elif (action == "cityChoice"):
-        area =  req.get("queryResult").get("parameters").get("city")
-        info = "您所選擇的旅是：" + city
 
     return make_response(jsonify({"fulfillmentText": info}))
 
