d93b4db9513caa50c66a7680fcb3d10dab5915dc
joy <yang10100520@gmail.com>
2023-05-04 14:48:16 +0800

    make it better

--- index.py
+++ index.py
@@ -17,26 +17,16 @@ def webhook():
     action =  req.get("queryResult").get("action")
     if (action == "cityChoice"):
         city =  req.get("queryResult").get("parameters").get("city")
-        info = "您所選擇的旅遊地區是：" + city
+        info = "您所選擇的旅遊地區是：" + city + "，相關景點:\n"
 
-#def get_link():
-
-    #db = firestore.client()
-
-    collection_ref = db.collection("台中")
-    #view = "高美濕地"
-    view = input("景點 : ")  # 用於來讀取想要前往的景點名稱
-    docs = collection_ref.where("view", "==", view).get()
-
-    for doc in docs:
-        result = doc.to_dict()
-        print("景點：" + result.get("view"))
-        print("景點介紹：" + result.get("introduction"))
-        print("地址：" + result.get("address"))
-        print("開放時間：" + result.get("time"))
-        print("票價：" + result.get("ticket"))
-
-#get_link()
+        collection_ref = db.collection("台中")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if city in dict["city"]:
+                result += "景點" + dict["title"]+"\n"
+            info += result
 
     return make_response(jsonify({"fulfillmentText": info}))
 
