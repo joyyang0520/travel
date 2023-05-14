988c54d2fac3d23bd3bb338e2f1d20398b559604
joy <yang10100520@gmail.com>
2023-05-14 19:37:11 +0800

    make it better

--- index.py
+++ index.py
@@ -3,13 +3,13 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
-import openai
-openai.api_key = os.getenv("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")
+#import openai
+#openai.api_key = os.getenv("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")
 from flask import Flask, request, make_response, jsonify
 app = Flask(__name__)
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    '''
+    
     req = request.get_json(force=True)
     
     #action =  req.get("queryResult").get("action")
@@ -32,15 +32,6 @@ def webhook():
             msg += "開放時間：" + result.get("time") + "\n"
             msg += "票價：" + result.get("ticket") + "\n\n"
     msg = info + "\n" + msg
-    '''
-
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="靜宜大學資管系楊子青老師在獲獎方面？",
-        max_tokens=500,
-        temperature=0.5,
-    )
-    msg = response.choices[0].text
 
     return make_response(jsonify({"fulfillmentText": msg}))
 if __name__ == "__main__":
