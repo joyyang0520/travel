97c4309d75ad6170dca1b86d7576385dab89423f
joy <yang10100520@gmail.com>
2023-05-14 14:00:19 +0800

    make it better

--- index.py
+++ index.py
@@ -13,29 +13,6 @@ app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    req = request.get_json(force=True)
-    #action =  req.get("queryResult").get("action")
-    #msg =  req.get("queryResult").get("queryText")
-    view =  req.get("queryResult").get("parameters").get("any")
-    info = "查詢內容：" + view
-
-    db = firestore.client()
-
-    collection_ref = db.collection("新北")
-    docs = collection_ref.get()
-
-    msg = ""
-    for doc in docs:
-        result = doc.to_dict()
-        if view in result.get("view"):
-            msg += "景點：" + result.get("view")+ "\n\n"
-            msg += "景點介紹：" + result.get("introduction")+ "\n\n"
-            msg += "地址：" + result.get("address") + "\n\n"
-            msg += "開放時間：" + result.get("time") + "\n\n"
-            msg += "票價：" + result.get("ticket") + "\n"
-
-    msg = info + "\n\n" + msg
-    return make_response(jsonify({"fulfillmentText": msg}))
 
     messages = ""
     while True:
