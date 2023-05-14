9d0109d048c0fe14147d2a2da9ad30e1905aa25b
joy <yang10100520@gmail.com>
2023-05-14 13:55:02 +0800

    make it better

--- index.py
+++ index.py
@@ -2,6 +2,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+import openai
+openai.api_key = os.getenv("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")
+
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
@@ -33,6 +36,20 @@ def webhook():
 
     msg = info + "\n\n" + msg
     return make_response(jsonify({"fulfillmentText": msg}))
+
+    messages = ""
+    while True:
+    msg = input('me > ')
+    messages.append({"role":"user","content":msg})   # 添加 user 回應
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        max_tokens=50,
+        temperature=0.5,
+        messages=messages
+    )
+    ai_msg = response.choices[0].message.content.replace('\n','')
+    messages.append({"role":"assistant","content":ai_msg})   # 添加 ChatGPT 回應
+    print(f'ai > {ai_msg}')
 
 if __name__ == "__main__":
     app.run()
