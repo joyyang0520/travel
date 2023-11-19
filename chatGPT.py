dd5cf3e3ee116f5e0079e45ff2aa438c67feb21f
joyyang0520 <yang10100520@gmail.com>
2023-11-19 23:29:02 +0800

    20231119 update API

--- chatGPT.py
+++ chatGPT.py
@@ -1,14 +1,14 @@
 import openai
 
-a_side = "sk-dbSdL9i73fTGBhZLMthxT3B"
-b_side = "lbkFJ35dB0WkQReqkeADsmWc4"
+a_side = "sk-AZ4lkfUPTEt5Bki2xv3KT3B"
+b_side = "lbkFJo6pnsChgFziv0QeUjLGm"
 openai.api_key = a_side + b_side
 
 def reply(msg):
     
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        max_tokens=100,
+        max_tokens=60,
         temperature=0.5,
         messages=[
             {"role": "assistant", "content": "我是負責處理旅遊問題的AI，我會簡短回答你所問的問題。"},
@@ -18,12 +18,12 @@ def reply(msg):
     remsg = response.choices[0].message.content
     
     if '。' or '.' in remsg:
-        remsg = remsg.split('。')[0]
+        remsg = remsg.split('。')[0] 
         
-    # print(remsg)
+    print(remsg)
     
     return remsg
-
-
-# reply("台灣哪裡有熱氣球可以搭")
-
+'''
+if __name__ == "__main__":
+    reply("台灣哪裡有熱氣球")
+'''
